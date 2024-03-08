2f69c1e984272a36c9797aebf2b5ad8295b3a57d
PedroHenriqueRochalevati <pepe.rolev@gmail.com>
2024-03-08 15:27:14 -0300

    att0803

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,18 +1,48 @@
-import { View } from "react-native";
-import Header from "../components/Header";
-import Stories from "../components/Stories";
-
+import { StyleSheet, Image, View, TouchableOpacity, Text, ScrollView } from 'react-native';
+import Header from '../components/Header';
+import Stories from '../components/Stories'
+import Post from '../components/Post';
+import postImage from '../assets/Foto1.jpg';
+import PessoaProfile from '../assets/pessoa2.webp';
 export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        marginVertical: 36,
-        marginHorizontal: 16,
-      }}
-    >
+    <ScrollView style={styles.container}>
       <Header />
       <Stories />
-    </View>
+      <Post
+        profileName={"Fulano da Silva"}
+        profileImage={PessoaProfile}
+        postImage={postImage}
+        description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla laoreet euismod lorem, vel tristique nisl egestas nec. In rutrum nibh in quam suscipit mollis. In accumsan egestas tellus, id porttitor risus dictum non. "} />
+      <Post
+        profileName={"Fulano da Silva"}
+        profileImage={PessoaProfile}
+        postImage={postImage}
+        description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla laoreet euismod lorem, vel tristique nisl egestas nec. In rutrum nibh in quam suscipit mollis. In accumsan egestas tellus, id porttitor risus dictum non. "} />
+      <Post
+        profileName={"Fulano da Silva"}
+        profileImage={PessoaProfile}
+        postImage={postImage}
+        description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla laoreet euismod lorem, vel tristique nisl egestas nec. In rutrum nibh in quam suscipit mollis. In accumsan egestas tellus, id porttitor risus dictum non. "} />
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'afff',
+    marginTop:35,
+    padding:5,
+    borderRadius:5,
+  },
+  borda:{
+    borderBottomWidth: 1,
+    opacity:5,
+    marginVertical:8,
+  
+  },
+  post: {
+    marginVertical:10,
+  },
+});
